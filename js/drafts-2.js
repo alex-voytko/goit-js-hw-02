b70ef60027fcce5c46716d99ae6b757b8f6bb84e
alex-voytko <voytko1994@gmail.com>
2020-12-15 14:49:17 +0200

    inception 1.1

---
+++ js/drafts-2.js
@@ -0,0 +1,11 @@
+const add = function(a, b) {
+    console.log('Hello');
+    console.log('a = ', a);
+    console.log('b = ', b);
+    const result = a + b;
+    console.log(result);
+};
+
+add(2, 5);
+
+add(33, 222);
---
+++ js/task-01.js
@@ -0,0 +1,13 @@
+// const country = ['Kyiv', 'Moscow', 'Milan', 'Paris'];
+// console.log(country);
+// console.log(country[2]);
+
+// country[2] = 'Gonduras';
+
+// console.log(country);
+// console.log(country[2]);   
+
+// for (let i = 0; i < country.length; i += 1) {
+//     // console.log({i+1}__{country[i]}``);
+//     console.log(`${i + 1} - ${country[i]}`)
+// }
