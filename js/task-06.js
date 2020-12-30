bc4f2c904d41ee40d0e0ab9c2d7f0f9f477142ec
alex-voytko <voytko1994@gmail.com>
2020-12-30 20:22:56 +0200

    final 2.3.3

--- js/task-06.js
+++ js/task-06.js
@@ -1,20 +1,20 @@
 let total = 0;
 const numbers = [];
 while (true) {
-    let input = prompt('Введите число:');
-    if (input === null) {
-        for (let i = 0; i < numbers.length; i += 1) {
-            total += numbers[i];
-        }
-        break;
-    }
-    if (input === isNaN(input)) {
-        alert('Было введено не число, попробуйте еще раз');
-        continue;
-    }
-    if (input === Number(input)) {
-        numbers.push(input);
-        continue;
-    }
+  let input = prompt('Введите число:');
+  if (input === isNaN(input)) {
+    alert('Было введено не число, попробуйте еще раз');
+    continue;
+  }
+  if (input === Number(input)) {
+    numbers.push(input);
+    continue;
+  }
+  if (input === null) {
+    break;
+  }
+}
+for (let i = 0; i < numbers.length; i += 1) {
+  total += numbers[i];
 }
 console.log(`Общая сумма чисел равна ${total}`);
